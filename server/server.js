c4a5c17d94c60ccadd9a3281169d953aee0e8e30
Nilou <nilou.houshmand@gmail.com>
2022-10-14 11:20:21 -0700

    setup controller and routes for user section:

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const router = require("./routes/userRoutes");
 
 const PORT = process.env.PORT;
 
 const app = express();
+
+app.use("/api/user", router);
+
 app.listen(PORT, () => {
 	console.log(`Ther server is running on ${PORT}`);
 });
