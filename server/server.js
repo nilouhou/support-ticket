b2e1df2f085ebd951c31ed2b1d7c7d30232ab131
Nilou <nilou.houshmand@gmail.com>
2022-10-14 10:40:55 -0700

    Basic Express setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-console.log("...server");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+app.listen(PORT, () => {
+	console.log(`Ther server is running on ${PORT}`);
+});
