a87617369f49c2eb9e073224397719bbe3966c87
Nilou <nilou.houshmand@gmail.com>
2022-10-17 15:53:12 -0700

    added protection to our get data route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controller/userController");
+const {
+	registerUser,
+	loginUser,
+	getMe,
+} = require("../controller/userController");
+
+const { protect } = require("../middleWares/authMiddleware");
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
+//added middleware to protect the route
+router.get("/login/me", protect, getMe);
 
 module.exports = router;
