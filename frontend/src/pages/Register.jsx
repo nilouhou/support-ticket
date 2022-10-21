a4162a207017d0e46f8e4d37f19f3c9a934e5749
Nilou <nilou.houshmand@gmail.com>
2022-10-21 14:00:21 -0700

    created Register UI

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,65 @@
+import { useState } from "react";
 function Register() {
-	return <div>Register</div>;
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		password: "",
+		matchPassword: "",
+	});
+
+	const { name, email, password, matchPassword } = formData;
+	return (
+		<>
+			<section>
+				<h1>Register</h1>
+				<p>Please Create a new account</p>
+			</section>
+			<section className="form">
+				<form>
+					<div className="form-group">
+						<input
+							type="text"
+							name="name"
+							id="name"
+							value={name}
+							placeholder="Please eneter your name"
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="email"
+							name="email"
+							id="email"
+							value={email}
+							placeholder="Please eneter your email"
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							name="password"
+							id="password"
+							value={password}
+							placeholder="Please eneter your password"
+						/>
+					</div>
+
+					<div className="form-group">
+						<input
+							type="password"
+							name="matchPassword"
+							id="matchPassword"
+							value={matchPassword}
+							placeholder="Please confirm your password"
+						/>
+					</div>
+					<div className="form-group">
+						<div className="btn">Register</div>
+					</div>
+				</form>
+			</section>
+		</>
+	);
 }
 
 export default Register;
