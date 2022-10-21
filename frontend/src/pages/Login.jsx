49c4320a9d4fe04634078ee1047caf232a5e39a4
Nilou <nilou.houshmand@gmail.com>
2022-10-21 14:54:26 -0700

    created login UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,59 @@
+import { useState } from "react";
 function Login() {
-	return <div>Login</div>;
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+	});
+
+	const { email, password } = formData;
+
+	const changeHandler = (e) => {
+		setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+	};
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+
+		console.log(formData);
+	};
+	return (
+		<>
+			<section>
+				<h1>Login</h1>
+				<p>Please Login into your account</p>
+			</section>
+			<section className="form">
+				<form onSubmit={submitHandler}>
+					<div className="form-group">
+						<input
+							type="email"
+							name="email"
+							id="email"
+							value={email}
+							placeholder="Please eneter your email"
+							onChange={changeHandler}
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							name="password"
+							id="password"
+							value={password}
+							onChange={changeHandler}
+							placeholder="Please eneter your password"
+						/>
+					</div>
+
+					<div className="form-group">
+						<button type="submit" className="btn btn-block">
+							Register
+						</button>
+					</div>
+				</form>
+			</section>
+		</>
+	);
 }
 
 export default Login;
