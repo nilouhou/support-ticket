0f162f0c0a319d3719e7cf92db5d7a55908c2a2e
Nilou <nilou.houshmand@gmail.com>
2022-10-27 10:09:52 -0700

    get user info from localstorage

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,8 +1,10 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import autoServer from "./authServer";
 
+const user = JSON.parse(localStorage.getItem("user"));
+
 const initialState = {
-	user: null,
+	user: user ? user : null,
 	isError: false,
 	isSucess: false,
 	isLoading: false,
