bd06e5d3f6c88ca150f4edca0a5f4fb9171fa5b5
Nilou <nilou.houshmand@gmail.com>
2022-11-11 10:50:01 -0800

    prepare to deploy to Heroku

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const colors = require("colors");
+const path = require("path");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const router = require("./routes/userRoutes");
@@ -23,6 +24,21 @@ app.use(express.urlencoded({ extended: false }));
 // Routes
 app.use("/api/users", router);
 app.use("/api/tickets", ticketRouter);
+
+//Serve FrontEnd
+if (process.env.Node_ENV === "production") {
+	// Set build folder as static
+	app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+	//any routes except the ones that we created should go this way
+	app.get("*", (req, res) =>
+		res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+	);
+} else {
+	app.get("/", (req, res) => {
+		res.status(200).json({ message: "Welcome to the Support Desk API" });
+	});
+}
 
 //Error Middleware
 app.use(errorHandler);
